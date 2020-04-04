1df93bfd5213fe814cff9aa0f2d346b58128932e
Morton <morton.nari@yahoo.com.au>
2020-04-04 11:26:53 +1100

    uploading my key

--- src/app/photo-gallery/photo-gallery.component.ts
+++ src/app/photo-gallery/photo-gallery.component.ts
@@ -9,11 +9,44 @@ import { ApiResponse } from '../models/ApiResponse';
   styleUrls: ['./photo-gallery.component.css']
 })
 export class PhotoGalleryComponent implements OnInit {
+
+  constructor(private flickrService: FlikrService) { }
   p = 1;
   errorMessage: string;
   flickrResponse: ApiResponse;
+  favorites: ApiResponse;
 
-  constructor(private flickrService: FlikrService) { }
+
+private ProChartStorage = {
+
+  getItem(key: string) {
+      return localStorage.getItem(key);
+  },
+
+  setItem(key: string, value: string) {
+     console.log('prochart setItem');
+     localStorage.setItem(key, value);
+  },
+
+  removeItem:  (key: string) => {
+      return localStorage.removeItem(key);
+  },
+
+  clear: () => {
+      const keys = new Array();
+      for (let i = 0, len = localStorage.length; i < len; i++) {
+          const key = localStorage.key(i);
+          if (key.indexOf('prochart') !== -1 || key.indexOf('ProChart') !== -1) {
+              keys.push(key);
+          }
+      }
+      // tslint:disable-next-line:no-shadowed-variable
+      for (let i = 0; i < keys.length; i++) {
+          localStorage.removeItem(keys[i]);
+      }
+  }
+
+};
   ngOnInit(): void {
     this.flickrService.getPhotos().subscribe({
       next: flickrResponse => this.flickrResponse = flickrResponse,
@@ -21,5 +54,33 @@ export class PhotoGalleryComponent implements OnInit {
       error: err => this.errorMessage = err,
     });
   }
+  store(isChecked: any, picId) {
+    if (isChecked ) {
+      console.log(isChecked, picId);
+      let pic = this.flickrResponse.photos.photo.find(obj => {
+        return obj.id === picId;
+        
+      });
+      const stringToStore = JSON.stringify(pic);
+      this.ProChartStorage.setItem('test.local.pic', stringToStore);
+
+      // for (const i in this.flickrResponse.photos.photo) {
+      //   if (this.flickrResponse.photos.photo[i].id === picId) {
+      //     console.log(this.flickrResponse.photos.photo[i]);
+      //     this.flickrResponse.photos.photo.[i].id.push({isSelected: true}); // {a: 5, b: 6};
+      //   }
+      // }
+    } else {
+      console.log('removed');
+      // this.favorites = this.favorites;
+      // const stringToStore = JSON.stringify(this.favorites);
+      // this.ProChartStorage.removeItem('test.local.favorites', stringToS);
+    }
+
+
+}
+
+
+
 
 }
--- src/app/services/flikr.service.ts
+++ src/app/services/flikr.service.ts
@@ -9,7 +9,7 @@ import { ApiResponse } from '../models/ApiResponse';
 })
 export class FlikrService {
   // tslint:disable-next-line:max-line-length
-  private imagesUrl = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=9d917c77afdd07ec80e68a6f8758abbc&contacts=&lat=-25.734968&lon=134.489563&format=json&nojsoncallback=1&api_sig=b001bbcf4f6b1116f0c025e76fe92a3e';
+  private imagesUrl = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a7404d4afa9adac1bff5c72fae1f226&lat=-25.734968&lon=134.489563&format=json&nojsoncallback=1';
   // flickerUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1';
   constructor(private http: HttpClient) { }
   getPhotos(): Observable<ApiResponse> {
